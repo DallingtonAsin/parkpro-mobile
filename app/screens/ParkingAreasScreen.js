e915ae625776f43103345b83ae4584a3deebbb04
DallingtonAsin <asingwire50dallington@gmail.com>
2022-06-10 00:08:06 +0300

    added icons on settings screen and disable alert for enable location on parking areas screen

--- app/screens/ParkingAreasScreen.js
+++ app/screens/ParkingAreasScreen.js
@@ -80,6 +80,8 @@ import {SafeAreaView,Dimensions,
         }
         
         const fetchNearByParkings = async() => {
+
+            // enable location please
             
             Geolocation.getCurrentPosition(
                 async(position) => {
@@ -98,7 +100,11 @@ import {SafeAreaView,Dimensions,
                         Toast.show(resp.message, Toast.LONG);
                     }
                     setIsLoading(false);
-                }, (error) => alert(error.message), { 
+                }, (error) =>
+                
+                console.log(error.message), 
+                
+                { 
                     enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 
                 }
                 );
@@ -419,7 +425,7 @@ import {SafeAreaView,Dimensions,
                                                 borderRadius:5,
                                             },
                                             container: {
-                                                marginTop: StatusBar.currentHeight,
+                                                // marginTop: StatusBar.currentHeight,
                                             },
                                             scene: {
                                                 flex: 1,
