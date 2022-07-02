f0425853fc490bf6e4113fba86f281e4fec10077
DallingtonAsin <asingwire50dallington@gmail.com>
2022-07-03 00:44:36 +0300

    redesigned buttons on parking fees screen

--- app/screens/ParkingFeesScreen.js
+++ app/screens/ParkingFeesScreen.js
@@ -80,7 +80,7 @@ const ParkingFeesScreen = ({route, navigation}) => {
     const EmptyFlastListMessage = ({item}) => {
         return (
             <Text style={styles.emptyListStyle}>
-               No fees Found
+            No fees Found
             </Text>
             );
         };
@@ -101,7 +101,7 @@ const ParkingFeesScreen = ({route, navigation}) => {
                 const addParkingToFavourites = async() => {
                     try {
                         if(parking_area_id){
-                    
+                            
                             const resp = await searchParkingArea(parking_area_id)
                             if(resp.statusCode == 1){
                                 const parking = resp.data[0];
@@ -205,10 +205,9 @@ const ParkingFeesScreen = ({route, navigation}) => {
                                 </Card.Content>
                                 </View>
                                 
-                                <Card.Actions>
-                                
+                             
                                 <View style={{
-                                    flex:1,
+                                    flex: 1,
                                     flexDirection: 'row',
                                     justifyContent: 'space-between',
                                     position: 'absolute',
@@ -216,9 +215,10 @@ const ParkingFeesScreen = ({route, navigation}) => {
                                     marginBottom:20,
                                 }}>
                                 
+                                <View style={styles.actionButtonView}>
                                 <TouchableOpacity
                                 onPress={() => setIsOpen(true) } 
-                                style={[styles.actionButton, {backgroundColor: colors.primary, borderColor: colors.primary}]}>
+                                style={[styles.actionButton, {backgroundColor: colors.primary}]}>
                                 <Text style={{ 
                                     color: colors.text,
                                     fontSize:14,
@@ -227,21 +227,22 @@ const ParkingFeesScreen = ({route, navigation}) => {
                                     Place request
                                     </Text> 
                                     </TouchableOpacity>
+                                    </View>
                                     
-                                    <TouchableOpacity style={[styles.actionButton, { left:20 }]} onPress={() =>  callHelpLine(phone_number)}>
-                                    <FontAwesome5 name="phone-alt" size={18} color={design.colors.dark}/>
+                                    <View style={styles.actionButtonView}>
+                                    
+                                    <TouchableOpacity style={styles.actionButton} onPress={() =>  callHelpLine(phone_number)}>
                                     <Text style={{
                                         fontSize:16, 
                                         paddingLeft:10, 
                                         fontWeight: 'bold',
                                         color:design.colors.dark
-                                        }}>Call</Text>
+                                    }}>Call</Text>
                                     </TouchableOpacity>
-                                    
-                                    
                                     </View>
                                     
-                                    </Card.Actions>
+                                    </View>
+
                                     </Card>
                                     </>
                                     : null
@@ -314,14 +315,18 @@ const ParkingFeesScreen = ({route, navigation}) => {
                                     alignItems: 'center',
                                     alignSelf: 'center',
                                 },
-                                actionButton:{
-                                    flexDirection: 'row',
-                                    height:50,
-                                    borderWidth:1,
-                                    borderRadius:30,
-                                    justifyContent: 'center',
+                                
+                                actionButtonView:{
+                                    paddingLeft:15,
+                                    paddingRight:15,
+                                    width: '50%'
+                                },
+
+                                actionButton: {
                                     alignItems: 'center',
-                                    width: 180
+                                    borderWidth: 1,
+                                    borderRadius: 30,
+                                    padding: 12,
                                 },
                                 
                                 tableCell:{
